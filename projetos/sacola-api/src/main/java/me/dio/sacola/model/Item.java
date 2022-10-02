0e9217ab8f9f0abc6f5eef7cefa825d22e4f4656
Andre Alencar <aaugusto310@gmail.com>
2022-10-02 07:07:36 -0300

    feat: implementação das classes com respectivos atributos e anotation do SpringBoot e lombok.

--- projetos/sacola-api/src/main/java/me/dio/sacola/model/Item.java
+++ projetos/sacola-api/src/main/java/me/dio/sacola/model/Item.java
@@ -1,4 +1,31 @@
 package me.dio.sacola.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private Produto produto;
+    private int quantidade;
+
+    @ManyToOne
+    @JsonIgnore
+    private Sacola sacola;
 }
--- projetos/sacola-api/src/main/java/me/dio/sacola/model/Sacola.java
+++ projetos/sacola-api/src/main/java/me/dio/sacola/model/Sacola.java
@@ -1,4 +1,37 @@
 package me.dio.sacola.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.dio.sacola.enumeration.FormaPagamento;
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
 public class Sacola {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JsonIgnore
+    private Cliente cliente;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Item> itens;
+
+    private double valorTotal;
+    @Enumerated
+    private FormaPagamento formaPagamento;
+
+    private boolean fechada;
 }
