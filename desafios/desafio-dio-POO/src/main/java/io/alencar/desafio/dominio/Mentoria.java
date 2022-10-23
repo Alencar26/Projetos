d6a4a2113c97d9949e1bfa6a6bf8c62f2771dc49
Andre Alencar <aaugusto310@gmail.com>
2022-10-23 00:54:04 -0300

    refactor: Refatorando classes para implementar herança.

--- desafios/desafio-dio-POO/src/main/java/io/alencar/desafio/dominio/Mentoria.java
+++ desafios/desafio-dio-POO/src/main/java/io/alencar/desafio/dominio/Mentoria.java
@@ -2,36 +2,23 @@ package io.alencar.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
 
-    private String titulo;
-    private String descricao;
     private LocalDate data;
 
     public Mentoria() {
     }
 
     public Mentoria(String titulo, String descricao, LocalDate data) {
-        this.titulo = titulo;
-        this.descricao = descricao;
+        super(titulo, descricao);
         this.data = data;
     }
 
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20d;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getData() {
         return data;
@@ -44,9 +31,9 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", data=" + data +
+                "titulo='" + this.getTitulo() + '\'' +
+                ", descricao='" + this.getDescricao() + '\'' +
+                ", data=" + this.getData() +
                 '}';
     }
 }
