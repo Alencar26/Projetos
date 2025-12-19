cca8bc80e8da952cd3dcf8981da17ae0a560a497
Alencar26 <aaugusto310@gmail.com>
2025-12-19 14:34:37 -0300

    feat(GO) URL Shortener - Redirect to mapped URL based on received path.
    Conceitos utilizados:
    - map;
    - slice;
    - struct;
    - parse YAML;
    - leitura de arquivo;
    - servido HTTP;
    - manipulação de bytes;
    - redirecionamento Web;
    
    A aplicação consiste em obter um path na requisição HTTP
    se esse path estiver mapeado a aplicação faz o redirecionamento para URL
    correspondente.

---
+++ projetos/URL-Shortener/go.mod
@@ -0,0 +1,5 @@
+module github.com/Alencar26/Projetos/projetos/URL-Shortener
+
+go 1.23.6
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ projetos/URL-Shortener/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/Alencar26/Projetos/projetos/URL-Shortener/urlshort"
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml, err := os.ReadFile("routes.yaml")
+	if err != nil {
+		panic(err)
+	}
+	// yaml := `
+	// - path: "/urlshort"
+	//   url: "https://github.com/gophercises/urlshort"
+	// - path: "/urlshort-final"
+	//   url: "https://github.com/gophercises/urlshort/tree/solution"
+	// `
+	yamlHandler, err := urlshort.YAMLHandler(yaml, mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	// http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
